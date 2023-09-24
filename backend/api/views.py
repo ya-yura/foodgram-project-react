2949dff9f0a8513a92d8518497667907c0368118
Yuri Pilov <zzz---zzz@yandex.ru>
2023-09-24 10:44:13 +0300

    Finalize optimization

--- backend/api/views.py
+++ backend/api/views.py
@@ -93,7 +93,6 @@ class CustomUserViewSet(ListRetrieveCreateViewSet):
         permission_classes=(permissions.IsAuthenticated,),
     )
     def subscribe(self, request, pk):
-        user = request.user
         following = get_object_or_404(User, pk=pk)
         self.check_object_permissions(request, following)
 
