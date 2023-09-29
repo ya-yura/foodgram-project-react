8547baae36431bbaa995793dc2f51b0f5b27921c
Yuri Pilov <zzz---zzz@yandex.ru>
2023-09-29 11:46:09 +0300

    6 review finalize

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.http import Http404
 
 from django_filters.rest_framework import DjangoFilterBackend
 
